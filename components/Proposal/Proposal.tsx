0e7c3aed85bfef45fafd7bf579233684a698bb29
damoski2 <akogunoyindamola42@gmail.com>
2022-06-29 15:36:52 +0100

    stash vote change

--- components/Proposal/Proposal.tsx
+++ components/Proposal/Proposal.tsx
@@ -4,6 +4,7 @@ import { noDataIllustration } from "../../Images";
 import { staticProposal, Proposal } from "../../static/staticProposal";
 import PrimaryButton from "../REUSABLES/PrimaryButton";
 import { trioCircle } from "../../Images";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 type Prop = {
   slug: string;
@@ -27,14 +28,34 @@ const Proposal: React.FC<Prop> = ({ slug }): JSX.Element => {
       <div className={style.vote__info}>
         <div className={style.vote__header}>
           <img src={trioCircle} alt="trioCircle" />
-          <h3>vote</h3>
+          <h2>vote</h2>
         </div>
         <div className={style.rule} />
         <div className={style.vote__details}>
-          <div className={style.vote__for}></div>
-          <div className={style.vote__against}></div>
+          <div className={style.vote__for}>
+            <div className={style.text}>
+              <p>Votes For</p>
+              <h2>10</h2>
+            </div>
+            <div className={style.progress__bar}>
+              <p>35%</p>
+              <ProgressBar completed={60} isLabelVisible={false} />
+            </div>
+          </div>
+          <div className={style.vote__against}>
+            <div className={style.text}>
+              <p>Votes For</p>
+              <h2>10</h2>
+            </div>
+            <div className={style.progress__bar}>
+              <p>35%</p>
+              <ProgressBar completed={60} isLabelVisible={false} />
+            </div>
+          </div>
         </div>
-        <h2>Proposer: <span>{proposal?.proposer ?? '0x00000000'}</span></h2>
+        <h2>
+          Proposer: <span>{proposal?.proposer ?? "0x00000000"}</span>
+        </h2>
       </div>
     );
   };
